5e013ef07426e6149e915f8063dd9557c41e263b
Kurt Lewis <kurt.jlewis15@gmail.com>
2019-02-04 22:12:05 -0500

    Add devenv script

--- tools/scripts.sh
+++ tools/scripts.sh
@@ -8,3 +8,25 @@ journal() {
   $EDITOR ~/gdrive/Journal/`date +%Y`/`date +%m-%d`.txt
   drive push -no-prompt ~/gdrive/Journal
 }
+
+
+########################
+# Tmux dev environment script
+########################
+# https://stackoverflow.com/a/40009032/7400944 
+# opens a tmux pane that looks like this
+# -------------
+# |     |     |
+# |     |     |
+# |     |-----|
+# |     |     |
+# -------------
+# opens  to the specified directory
+devenv() {
+  tmux new-session \; \
+  send-keys 'cd ' $1 '; clear' C-m \; \
+  split-window -h \; \
+  send-keys 'cd ' $1 '; clear' C-m \; \
+  split-window -v -p 35 \; \
+  send-keys 'cd ' $1 '; clear' C-m \; \
+}
