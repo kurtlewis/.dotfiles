37dfa4afc5712989c261acabcc7f19ad323ee0f3
Kurt Lewis <kurt.jlewis15@gmail.com>
2019-02-18 20:21:23 -0500

    Add tmux conf

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 ln -sv ~/.dotfiles/git/.gitconfig ~
 ln -sv ~/.dotfiles/vim/ ~/.vim
+ln -sv ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
 
 echo ""
 echo "To install tools scripts add the following line to your ~/.zshrc"
